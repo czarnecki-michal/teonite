aa88a09966b0a35cf9a8ea47855e370f6f729fa1
Michal Czarnecki <mczarnecki11@gmail.com>
2019-08-04 17:29:51 +0200

    Added urls for stats and stats/<author>

--- scraper/articles/urls.py
+++ scraper/articles/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('articles/', views.ArticleList.as_view()),
     path('articles/<int:pk>/', views.ArticleDetail.as_view()),
     path('authors/', views.AuthorList.as_view()),
-    path('authors/<int:pk>/', views.AuthorDetail.as_view()),
+    path('stats/', views.show_stats, name='stats'),
+    path('stats/<author>', views.show_author_stats, name='author_stats')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
