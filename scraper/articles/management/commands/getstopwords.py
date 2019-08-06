94ad166a41e4778535c3027e9e7bf8cfaf702c41
Michał Czarnecki <nwasm30@gmail.com>
2019-08-06 14:55:05 +0200

    Removed stopwords command

--- scraper/articles/management/commands/getstopwords.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-import nltk
-
-class Command(BaseCommand):
-    help = 'Downloads nltk stopwords'
-
-    def handle(self, *args, **options):
-        nltk.download("stopwords")
