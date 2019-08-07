ce7562ea76814b748086cb5d20d29946428ac61d
Michał Czarnecki <nwasm30@gmail.com>
2019-08-07 15:06:09 +0200

    Fix

--- scraper/articles/apps.py
+++ scraper/articles/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
+import nltk
 
 
 class ArticlesConfig(AppConfig):
     name = 'articles'
 
     def ready(self):
-        import nltk
         nltk.download("stopwords")
