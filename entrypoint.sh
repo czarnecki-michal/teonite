8ccfca5b742d04c5d33c41a9f89cab9ed068e577
Michał Czarnecki <mczarnecki11@gmail.com>
2019-08-11 16:30:16 +0200

    Modified docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py makemigrations core
 python manage.py migrate core
 python manage.py download_stopwords
 python manage.py insertdata
-python manage.py runserver localhost:8080
+python manage.py runserver 0.0.0.0:8080
